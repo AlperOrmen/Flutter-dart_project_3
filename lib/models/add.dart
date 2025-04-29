5f971cff496d272a70d6c1c0ac55719e4cda0adb
Nazwper <alperormen@gmail.com>
2025-04-29 21:56:27 +0300

    Add files via upload
    flutter-dart calculating the avarage of the given numbers by user app | reupload from 04.05.2023

---
+++ lib/models/add.dart
@@ -0,0 +1,7 @@
+class Models  {
+  int id;
+  String baslik;
+  int derskredi;
+  int dersnotu; 
+  Models({required this.id, required this.baslik, required this.derskredi, required this.dersnotu});
+}
